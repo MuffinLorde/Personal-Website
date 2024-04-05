a9ff237347da2995ed8cd067562c23966b309685
MuffinLorde <scromskrom@gmail.com>
2024-04-05 17:17:10 +0200

    commit

--- other/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // You can add your own logic for form submission here, like sending data to a server
-    alert('Form submitted!');
-  });
-  
